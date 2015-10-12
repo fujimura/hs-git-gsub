e53cb9d0aa5291e15bf7b2232a32c8b11bfbff32
Fujimura Daisuke <me@fujimuradaisuke.com>
2015-10-13 01:26:16 +0900

    Implement MVP

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
-import Lib
+import           Data.Maybe         (listToMaybe)
+import           System.Environment (getArgs)
+
+import qualified Lib
 
 main :: IO ()
-main = someFunc
+main = getArgs >>= run
+
+run :: [String] -> IO ()
+run []             = error "No argument to gsub was given"
+run ["-v"]         = showVersion
+run ["--version"]  = showVersion
+run [_]            = error "Please specify `to`"
+run (from:to:path) = Lib.run from to (listToMaybe path)
+
+showVersion :: IO ()
+showVersion = putStrLn "0.0.1" -- TODO
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,46 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Control.Monad
+import           Data.Maybe       (maybeToList)
+import           Data.Text        (Text)
+import qualified Data.Text        as T
+import qualified Data.Text.IO     as T
+import           System.Directory (doesDirectoryExist, getDirectoryContents)
+import           System.Exit
+import           System.FilePath  (getSearchPath)
+import           System.IO        (hClose)
+import           System.IO.Temp   (withSystemTempFile)
+import           System.Process   (callProcess, readProcessWithExitCode)
+
+run :: String -> String -> Maybe FilePath -> IO ()
+run from to path = do
+  targets <- getTargetFiles from path
+  mapM_ (substitute' from to) targets
+
+getTargetFiles :: String -> Maybe FilePath -> IO [FilePath]
+getTargetFiles from path = do
+  (_,result,_) <- readProcessWithExitCode "git" (["grep", "-l", from] ++ maybeToList path ) []
+  return (lines result)
+
+substitute :: String -> -- From
+              String -> -- To
+              FilePath -> -- File
+              IO ()
+substitute from to file =
+    T.replace (T.pack from) (T.pack to) <$> T.readFile file >>= T.writeFile file
+
+substitute' :: String -> -- From
+              String -> -- To
+              FilePath -> -- File
+              IO ()
+substitute' from to file = do
+   original <- T.readFile file
+   let changed = T.replace (T.pack from) (T.pack to) original
+   withSystemTempFile (file ++ ".") $ \tmpFile hFile -> do
+     T.hPutStr hFile changed
+     hClose hFile
+     (_,diff,_) <- readProcessWithExitCode "git" ["diff", "--no-index", "--color", file, tmpFile] []
+     putStrLn diff
+     putStrLn "Apply this change?(y/n)"
+     answer <- getChar
+     when (answer == 'y') $ T.replace (T.pack from) (T.pack to) <$> T.readFile file >>= T.writeFile file
