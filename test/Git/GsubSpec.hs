485c2e8953bd01aeb83a80f51e13cbab05c770e4
Fujimura Daisuke <me@fujimuradaisuke.com>
2015-10-28 02:27:58 +0900

    Minor cleanup

--- test/Git/GsubSpec.hs
+++ test/Git/GsubSpec.hs
@@ -13,22 +13,21 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
-  around_ (hSilence [stdout] . inTempRepo) $ do
-    it "should substitute" $ do
-      Cli.run ["foo", "bar"]
-      actual <- readFile "foo.rb"
-      actual `shouldContain` "def bar"
+spec = around_ (hSilence [stdout] . inTempRepo) $ do
+  it "should substitute" $ do
+    Cli.run ["foo", "bar"]
+    actual <- readFile "foo.rb"
+    actual `shouldContain` "def bar"
 
-    it "can specify directory" $ do
-      Cli.run ["foo", "bar", "foo"]
-      actual <- readFile "foo/bar.rb"
-      actual `shouldContain` "def bar"
+  it "can specify directory" $ do
+    Cli.run ["foo", "bar", "foo"]
+    actual <- readFile "foo/bar.rb"
+    actual `shouldContain` "def bar"
 
-      actual' <- readFile "foo.rb"
-      actual' `shouldNotContain` "def bar"
+    actual' <- readFile "foo.rb"
+    actual' `shouldNotContain` "def bar"
 
-    it "should substitute interactively" $ do
-      runWithStdin "y" $ Cli.run ["-i", "foo", "bar"]
-      actual <- readFile "foo.rb"
-      actual `shouldContain` "def bar"
+  it "should substitute interactively" $ do
+    runWithStdin "y" $ Cli.run ["-i", "foo", "bar"]
+    actual <- readFile "foo.rb"
+    actual `shouldContain` "def bar"
