c662c45140f27b309d12bee8d79069fa5f3fc0b2
Fujimura Daisuke <me@fujimuradaisuke.com>
2015-10-28 22:19:15 +0900

    Test -v and --version

--- test/Git/GsubSpec.hs
+++ test/Git/GsubSpec.hs
@@ -3,10 +3,14 @@
 module Git.GsubSpec ( main, spec ) where
 
 import qualified Cli
+import qualified Paths_hs_git_gsub
 
+import           Control.Exception  (catch)
+import           Data.Version       (showVersion)
 import           Helper
+import           System.Exit        (ExitCode (ExitSuccess))
 import           System.IO          (stdout)
-import           System.IO.Silently (hSilence)
+import           System.IO.Silently (capture, hSilence)
 import           Test.Hspec
 
 main :: IO ()
@@ -31,3 +35,9 @@ spec = around_ (hSilence [stdout] . inTempRepo) $ do
     runWithStdin "y" $ Cli.run ["-i", "foo", "bar"]
     actual <- readFile "foo.rb"
     actual `shouldContain` "def bar"
+
+  it "should show version" $ do
+    let run args = fst <$> (capture $ Cli.run args `catch` (\ExitSuccess -> return ()))
+
+    run ["-v"] `shouldReturn` (showVersion Paths_hs_git_gsub.version) ++ "\n"
+    run ["--version"] `shouldReturn` (showVersion Paths_hs_git_gsub.version) ++ "\n"
