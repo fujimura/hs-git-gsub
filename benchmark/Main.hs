059d616c3ce4bd99573c83444691538721c9f7c7
Daisuke Fujimura <me@fujimuradaisuke.com>
2024-03-24 23:44:06 +0900

    TEST

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,13 +1,9 @@
-import qualified Cli
 import Criterion.Main
 import System.Directory
-import System.Process
 
 benchmark1 :: IO ()
 benchmark1 = do
   withCurrentDirectory "benchmark/git" $ do
-    Cli.run ["git", "svn"]
-    (exitcode, _, _) <- readProcessWithExitCode "git" ["reset", "--hard"] []
     return ()
 
 main :: IO ()
